c148b512ce2025b9aeb821641fe21b188baf2ae3
Sergey Breusov <bsa2609@yandex.ru>
2024-09-08 19:13:14 +0300

    add Url model and url database structure

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,8 +2,15 @@ package hexlet.code;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinJte;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
 public class App {
     private static int getPort() {
@@ -18,8 +25,16 @@ public class App {
     public static String getDataBaseUrl() {
         return System.getenv().getOrDefault(
                 "JDBC_DATABASE_URL",
-                "jdbc:h2:mem:java_project_72;DB_CLOSE_DELAY=-1;"
+                "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;"
         );
+    }
+
+    private static String readResourceFile(String fileName) throws IOException {
+        var inputStream = App.class.getClassLoader().getResourceAsStream(fileName);
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
     }
 
     public static Javalin getApp() throws Exception {
@@ -27,6 +42,18 @@ public class App {
         hikariConfig.setJdbcUrl(getDataBaseUrl());
 
         var dataSource = new HikariDataSource(hikariConfig);
+        var sql = readResourceFile("schema.sql");
+
+        if (System.getenv("JDBC_DATABASE_URL") != null) {
+            sql = sql.replaceAll("AUTO_INCREMENT", "GENERATED ALWAYS AS IDENTITY");
+        }
+
+        try (var connection = dataSource.getConnection();
+             var statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
+
+        BaseRepository.dataSource = dataSource;
 
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
---
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -0,0 +1,30 @@
+package hexlet.code.model;
+
+import java.time.LocalDateTime;
+
+public class Url {
+    private Long id;
+    private String name;
+    private LocalDateTime createdAt;
+
+    public Url(String name) {
+        this.name = name;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+}
